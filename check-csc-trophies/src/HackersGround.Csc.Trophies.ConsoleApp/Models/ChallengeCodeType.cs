80d24bb109d453068ba5220d3819679ac84f2038
zihyeon08 <104641422+zihyeon08@users.noreply.github.com>
2024-07-05 00:56:05 +0900

    Update GHA for CSC completion submitted

---
+++ check-csc-trophies/src/HackersGround.Csc.Trophies.ConsoleApp/Models/ChallengeCodeType.cs
@@ -0,0 +1,22 @@
+﻿namespace HackersGround.Csc.Trophies.ConsoleApp.Models;
+
+/// <summary>
+/// This defines the challenge code type.
+/// </summary>
+public enum ChallengeCodeType
+{
+    /// <summary>
+    /// Indicates the challenge code is undefined.
+    /// </summary>
+    Undefined,
+
+    /// <summary>
+    /// Indicates the challenge code is AZ-900.
+    /// </summary>
+    AZ_900,
+
+    /// <summary>
+    /// Indicates the challenge code is AI-900.
+    /// </summary>
+    AI_900,
+}
