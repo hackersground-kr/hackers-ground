8dec681b00b9825d7c7c281480456cf7fd3ee3a5
zihyeon08 <104641422+zihyeon08@users.noreply.github.com>
2024-07-28 15:21:28 +0900

    Update GHA for workshop submission & completion

---
+++ check-workshop-challenge/src/HackersGround.Workshop.Challenge.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using HackersGround.Workshop.Challenge.ConsoleApp.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+               .UseConsoleLifetime()
+               .ConfigureServices(services =>
+               {
+                   services.AddHttpClient<IChallengeCheckerService, ChallengeCheckerService>(p =>
+                   {
+                       p.Timeout = TimeSpan.FromSeconds(240);
+                   });
+               })
+               .Build();
+
+var service = host.Services.GetRequiredService<IChallengeCheckerService>();
+await service.RunAsync(args);
---
+++ check-workshop-challenge/test/HackersGround.Workshop.Challenge.ConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HackersGround.Workshop.Challenge.ConsoleApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
